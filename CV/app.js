91af085a3a7d1e2e1f29b52ff36e0b1ccdb7a0b2
KevinPgn <68778821+KevinPgn@users.noreply.github.com>
2022-02-10 09:03:40 +0100

    Delete CV directory

--- CV/app.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.burger')
-const menu = document.querySelector('.right')
-
-burger.addEventListener('click', () => {
-
-    menu.classList.toggle('active')
-
-})
